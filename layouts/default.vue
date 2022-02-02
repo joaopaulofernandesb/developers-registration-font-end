df18afa527b2debea1e21b32549d328a9d498fae
João Paulo Fernandes Brasil <joao.brasil@joao.brasil>
2022-02-02 01:20:56 -0300

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -73,9 +73,9 @@ export default {
           to: '/',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          icon: 'mdi-chart-timeline',
+          title: 'Level',
+          to: '/level',
         },
       ],
       miniVariant: true,
